f02a29526061df2334abec8f21803fe82ce4428f
Rafael Yukio Kanaoka <rkanaoka2@gmail.com>
2026-02-18 01:20:49 +0000

    database status page

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,28 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1+1 as sum;");
-  console.log(result.rows[0].sum); // Should log 2
-  response.status(200).json({
-    status: "ok",
-    timestamp: new Date().toISOString(),
+
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseMaxConnections = await database.query("SHOW max_connections;");
+
+  //const databaseName = request.query.dbname || process.env.POSTGRES_DB;
+  //const databaseCurrentConnections = await database.query(`SELECT COUNT(*) FROM pg_stat_activity where datname = '${databaseName}';`);
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseCurrentConnections = await database.query({
+    text: "SELECT COUNT(*) FROM pg_stat_activity where datname = $1;",
+    values: [databaseName],
+  });
+
+  response.status(200).json({ 
+    updated_at: new Date().toISOString(),
+    dependencies: {
+      database: {
+      version: databaseVersionResult.rows[0].server_version,
+      max_connections: databaseMaxConnections.rows[0].max_connections,
+      current_connections: databaseCurrentConnections.rows[0].count,
+    }
+    }, 
   });
 }
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -2,3 +2,27 @@ test("API status returns 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
 });
+
+test("Database variables", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
+  const responseJson = await response.json();
+
+  const databaseVersion = responseJson.dependencies.database.version;
+  const databaseMaxConnections = responseJson.dependencies.database.max_connections;
+  const databaseCurrentConnections = responseJson.dependencies.database.current_connections;
+
+  expect(databaseVersion).toBeDefined();
+  expect(databaseVersion).toBe("16.0");
+
+  expect(databaseMaxConnections).toBeDefined();
+  expect(databaseMaxConnections).toBe("100");
+
+  expect(databaseCurrentConnections).toBeDefined();
+  expect(parseInt(databaseCurrentConnections)).toBe(1);
+
+});
+
+test("Database SQL Injection", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status?dbname=local_db");
+  //const response = await fetch("http://localhost:3000/api/v1/status?dbname='; SELECT pg_sleep(2); --");
+});
